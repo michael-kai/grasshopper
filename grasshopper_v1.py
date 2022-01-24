d91ac0477d026dc8010872a83e27cb1fc8bece5a
michael-kai <71984697+michael-kai@users.noreply.github.com>
2022-01-25 00:09:30 +0200

    refactored

--- grasshopper_v1.py
+++ grasshopper_v1.py
@@ -3,8 +3,6 @@ import requests
 import re
 import openpyxl
 from openpyxl import Workbook
-
-
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
            'accept': '*/*'}
@@ -12,15 +10,14 @@ HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
 COLUMN_TITLES = ['URL', 'Status code', 'Sponsored', 'NoFollow', 'UGC', 'Links Status']
 
 
-def open_excel():
+def openExcel():
     open_book = openpyxl.open('excel_file.xlsx', read_only=True)
     sheet = open_book.active
     url_list = [x[1] for x in sheet.values]
     acceptor_domain = str(sheet['A1'].value).replace('https:', '').replace('/', '')
     return acceptor_domain, url_list
 
-
-def check_link_attrs(href):
+def checkLinkAttrs(href):
     links_attrs = [None, None, None]
     if 'ugc' in href:
         links_attrs[0] = 'Found'
@@ -30,15 +27,13 @@ def check_link_attrs(href):
         links_attrs[2] = 'Found'
     return links_attrs
 
+def formatResult(links_attrs, url, status_code):
+    format_result = [url, status_code]
+    format_result.extend(links_attrs)
+    format_result.append('Links found')
+    return format_result
 
-def format_result(links_attrs, url, status_code):
-    format_result_list = [url, status_code]
-    format_result_list.extend(links_attrs)
-    format_result_list.append('Links found')
-    return format_result_list
-
-
-def get_page_data(url_list, acceptor_domain):
+def getPageData(url_list, acceptor_domain):
     result = []
     for url in url_list:
         page = requests.get(url, headers=HEADERS, allow_redirects=False)
@@ -46,9 +41,9 @@ def get_page_data(url_list, acceptor_domain):
             soup = BeautifulSoup(page.text, 'html.parser')
             hrefs = str(soup.find_all(href=re.compile(acceptor_domain)))
             if len(hrefs) > 2:
-                links_attrs = check_link_attrs(hrefs)
-                format_result_list = format_result(links_attrs, url, page.status_code)
-                result.append(format_result_list)
+                links_attrs = checkLinkAttrs(hrefs)
+                format_result = formatResult(links_attrs, url, page.status_code)
+                result.append(format_result)
             else:
                 no_links = [url, page.status_code, None, None, None, 'Links not found']
                 result.append(no_links)
@@ -57,16 +52,16 @@ def get_page_data(url_list, acceptor_domain):
             result.append(not_200)
     return result
 
-
-def create_result_file(result_list):
+def createResultFile(result_list):
     result_book = Workbook()
     result_book_sheet = result_book.active
     result_book_sheet.append(COLUMN_TITLES)
     for row in result_list:
         result_book_sheet.append(row)
+
     result_book.save('Result_list.xlsx')
 
 
-acceptor_domain, url_list = open_excel()
-result_list = get_page_data(url_list, acceptor_domain)
-create_result_file(result_list)
+acceptor_domain, url_list = openExcel()
+result_list = getPageData(url_list, acceptor_domain)
+createResultFile(result_list)
